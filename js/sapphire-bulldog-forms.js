a4f3bc7412812cae886b688c238b7fd9d84885f4
John Rake <john.rake12@gmail.com>
2013-01-26 23:43:47 -0600

    connectivity between the js and a form is complete. Begun work on actual validation cases now.

--- js/sapphire-bulldog-forms.js
+++ js/sapphire-bulldog-forms.js
@@ -22,31 +22,163 @@
 }());
 
 // Main
-function sbf(args) {
-    this.version     = 'v1.0.0';
-    this.form_id     = args['form_id'];
-    this.form        = {};
-    this.form_fields = [];
+function sbf( args ) {
+    this.version         = 'v1.0.0';
+    this.debug           = args['debug'];
+    this.formId          = args['formId'];
+    this.form            = {};
+    this.formFields      = {}; // form input fields 
+    this.validationTypes = [ 'required', 'maxlen' ];
+    this.errorMessages   = [];
+    this.errorLocation   = args['errorLocation'];
 
     this.init();
 }
 
 sbf.prototype.init = function() {
-    this.form = document.getElementById( this.form_id );
-    this.form_fields = this.toArray( this.form.getElementsByTagName( 'input' ) );
+    this.form = document.getElementById( this.formId );
+    this.form.onsubmit = this.validate;
 
-    for ( var i in this.form_fields ) {
-        if ( this.form_fields[i].type === 'submit' ) {
-            this.form_fields.splice(i);
+    for ( var i in this.form.elements ) {
+        if ( this.form.elements[i].type !== 'submit' &&
+             this.form.elements[i].id   !== undefined ) {
+            this.formFields[this.form.elements[i].id] = { 'element': this.form.elements[i] };
         }
     }
-}
 
-sbf.prototype.toArray = function(obj) {
+    //log current form fields
+    this.log( 'form fields for >' + this.formId + '< form' );
+    this.log( this.formFields );
+
+    window.sbf[this.formId] = this; // hold this form's init data in window for use later in validation
+};
+
+sbf.prototype.addValidation = function( fieldName, validation ) {
+    var field = this.formFields[fieldName];
+
+    if ( field === undefined || field === null ) {
+        this.log( 'unable to find the field >' + fieldName + '<' );
+        return false;
+    }
+
+    if ( typeof validation !== 'string' )  {
+        this.log( 'validation must be a string, found >' + typeof validation + '<' );
+        return false;
+    }
+
+    var valArray = validation.split( '=' );
+    var length   = this.validationTypes.length;
+
+    var success = false;
+    while ( length-- ) {
+        if ( this.validationTypes[length] === valArray[0] ) {
+            // validation type confirmed, lets see if we need to add the validation section now
+            if ( field['validation'] === undefined ) {
+                field['validation'] = {};
+            }
+
+            if ( valArray.length === 2 ) {
+                field['validation'][valArray[0]] = valArray[1];
+                success = true;
+            } else if ( valArray.length === 1 ) {
+                field['validation'][valArray[0]] = true;
+                success = true;
+            } else {
+                return false;
+            }
+        }
+    }
+
+    if ( success ) {
+        this.log( 'added >' + validation + '< validation to the field >' + fieldName + '<' );
+    }
+
+    return success;
+};
+
+sbf.prototype.validate = function() {
+    formObj = window.sbf[this.id];
+
+    formObj.errorMessages = []; // reset error messages that may be left from previous submit atempts
+
+    var success = true;
+
+    for ( var i in formObj.formFields ) {
+        if ( formObj.formFields[i]['validation'] ) {
+            var field = formObj.formFields[i];
+            var tagName = field['element'].tagName.toUpperCase();
+
+            for ( var validationType in field['validation'] ) {
+                var validationValue = field['validation'][validationType];
+
+                switch ( validationType ) {
+                    case 'required':
+                        if ( tagName === 'INPUT' && formObj.isTextField(field['element']) ) {
+                            if ( field['element'].value.length === 0 ) {
+                                formObj.errorMessages.push( field['element'].id + ' is required' );
+                                success = false;
+                            }
+                        }
+                        break;
+                    case 'maxlen':
+                        if ( tagName === 'INPUT' && formObj.isTextField(field['element']) ) {
+                            if ( field['element'].value.length > validationValue ) {
+                                formObj.errorMessages.push( field['element'].id + ' is required' );
+                            }
+                        }
+                        break;
+                    default:
+                        formObj.log( 'unknown valition type >' + validationType + '<' );
+                        break;
+                }
+            }
+        }
+    }
+
+    formObj.displayErrors();
+
+    return success;
+};
+
+sbf.prototype.displayErrors = function() {
+    var fullErrorMessage = '';
+
+    for ( var i in this.errorMessages ) {
+        fullErrorMessage = fullErrorMessage + this.errorMessages[i] + '\n';
+    }
+
+    switch ( this.errorLocation ) {
+        case 'inline':
+            break;
+        case 'preform':
+            break;
+        case 'postform':
+            break;
+        default:
+            alert( fullErrorMessage );
+            break;
+    }
+};
+
+sbf.prototype.isTextField = function( field ) {
+    if ( field.type === 'text' || field.type === 'textarea' || field.type === 'password' ) {
+        return true;
+    } else {
+        return false;
+    }
+};
+
+sbf.prototype.log = function( message ) {
+    if ( this.debug ) {
+        console.debug( message );
+    }
+};
+
+sbf.prototype.toArray = function( obj ) {
     var array = [];
     // iterate backwards ensuring that length is an UInt32
-    for (var i = obj.length >>> 0; i--;) { 
+    for ( var i = obj.length >>> 0; i--; ) { 
         array[i] = obj[i];
     }
     return array;
-}
+};
